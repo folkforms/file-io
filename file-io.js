46627722f53cd42cea2709cfa5dd4eb8dfca2911
folkforms <alan.bergin@gmail.com>
2021-05-31 21:49:05 +0100

    Allow writeLines to take either string or array data

--- file-io.js
+++ file-io.js
@@ -39,12 +39,17 @@ const readJson = filename => {
  * Writes the given array to a file.
  *
  * @param {string} filename file to write
- * @returns {array} array array of lines to write
+ * @returns {array} data string or array of lines to write
  */
-const writeLines = (filename, array, append = false) => {
-  const data = array.join("\n");
+const writeLines = (filename, data, append = false) => {
+  let dataOut;
+  if(typeof data === "string") {
+    dataOut = data;
+  } else {
+    dataOut = data.join("\n");
+  }
   const options = { flag: append ? "a" : "w" };
-  fs.outputFileSync(filename, data, options);
+  fs.outputFileSync(filename, dataOut, options);
 }
 
 const fileio = { globFiles, readLines, readJson, writeLines };
